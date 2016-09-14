47191048d0beab84f634e15387ba265d89b6df32
Yan <Yan@Yan.local>
2016-09-14 17:20:22 -0400

    alignment fix

--- index.php
+++ index.php
@@ -38,16 +38,16 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">Yan Chen</a>
+              <a class="navbar-brand" href="#">PROJECTS</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
                 <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About <span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Education</a></li>
-                    <li><a href="#">Resume</a></li>
+                    <li><a href="#">Project 2</a></li>
+                    <li><a href="#">Project 3</a></li>
+                    <li><a href="#">Project 4</a></li>
                   </ul>
                 </li>
               </ul>
@@ -77,36 +77,35 @@
           <p class="lead"> Some stuff about me</p>
         </div>
         <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block" src="images/Me.JPG" alt="IMAGE OF YAN" width="500" height="500">
+          <img class="featurette-image img-responsive center-block" src="images/Me.JPG" alt="IMAGE OF YAN" width="300">
         </div>
       </div>
+    </div>
 
       <hr class="featurette-divider">
 
       <!-- /END THE FEATURETTES -->
 
       <!-- Three columns of text below the carousel -->
-      <div class="row">
-        <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Project 1</h2>
-          <p><a href =" ">View</a></p>
-          <p><a href =" ">Github</a></p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
+      <div class="container marketing">
+      <div class="row" align="center">
         <div class="col-lg-4">
           <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
           <h2>Project 2</h2>
-          <p><a href =" ">View</a></p>
-          <p><a href =" ">Github</a></p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-default" href="#" role="button">View &raquo;</a></p>
+          <p><a class="btn btn-default" href="#" role="button">Github &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
           <h2>Project 3</h2>
-          <p><a href =" ">View</a></p>
-          <p><a href =" ">Github</a></p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-default" href="#" role="button">View &raquo;</a></p>
+          <p><a class="btn btn-default" href="#" role="button">Github &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+          <h2>Project 4</h2>
+          <p><a class="btn btn-default" href="#" role="button">View &raquo;</a></p>
+          <p><a class="btn btn-default" href="#" role="button">Github &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
 
