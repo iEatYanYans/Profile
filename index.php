e1e05095bb1f314a45b4e0950760951f5d473179
Yan <Yan@Yan.home>
2016-09-15 20:38:50 +0100

    changed description

--- index.php
+++ index.php
@@ -43,20 +43,9 @@
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li class="dropdown-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
+                <li><a href="#Project2">Project 2</a></li>
+                <li><a href="#Project3">Project 3</a></li>
+                <li><a href="#Project4">Project 4</a></li>
               </ul>
             </div>
           </div>
@@ -80,8 +69,8 @@
 
       <div class="row featurette">
         <div class="col-md-7">
-          <h2 class="featurette-heading" align="center">Yan Chen <span class="text-muted"></span></h2>
-          <p class="lead" align="center"> Some stuff about me</p>
+          <h2 class="featurette-heading">Yan Chen <span class="text-muted">: A Dynamic Web App Student</span></h2>
+          <p class="lead"> Hey everyone! My name is Yan (pronounced Y-AN) and I'm currently enrolled in CSCI E-15, welcome to my homepage!</p>
         </div>
         <div class="col-md-5">
           <img class="featurette-image img-responsive center-block" src="images/Me.JPG" alt="IMAGE OF YAN" width="500">
