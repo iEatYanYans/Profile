ea074ad247a1cdcd47a34a1b81aa0e9509ef90d6
Yan <Yan@Yan.home>
2016-09-15 20:28:36 +0100

    dropdown fix

--- index.php
+++ index.php
@@ -42,12 +42,19 @@
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
                 <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About <span class="caret"></span></a>
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="#">Project 2</a></li>
-                    <li><a href="#">Project 3</a></li>
-                    <li><a href="#">Project 4</a></li>
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li class="dropdown-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
                   </ul>
                 </li>
               </ul>
@@ -108,6 +115,8 @@
           <p><a class="btn btn-default" href="#" role="button">Github &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
+
+        <hr class="featurette-divider">
 
 
 
